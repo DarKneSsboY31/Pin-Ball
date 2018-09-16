4b0993a2c581b57d83914be642d0b35acf74da9d
DarKneSsboY31 <blackimpact0123@outlook.jp>
2018-09-16 09:14:23 +0900

    Pin Ballプロジェクト　初めてのコミット
    PinBallのGame Sceneを作成したファイルです。

---
+++ Assets/FripperController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+    //HingeJointコーポネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20f;
+    //はじいた時の傾き
+    private float flickAngle = -20f;
+
+    // Use this for initialization
+    void Start () {
+
+        //HingeJointコンポーネントの取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //左矢印キーを押した時、左フリッパーを動かす。
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+            SetAngle(this.flickAngle);
+        }
+
+        //右矢印キーを押した時、右フリッパーを動かす。
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") {
+            SetAngle(this.flickAngle);
+        }
+
+        //矢印キーが離された時、フリッパーをそれぞれ元に戻す。
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag"){
+            SetAngle(this.defaultAngle);
+        }
+
+        //画面左をタップした時、左フリッパーを動かす。
+        if (Input.GetMouseButtonDown(0) && tag == "LeftFripperTag" && Input.GetTouch(0).position.x < Screen.width * 0.5f) {
+            SetAngle(this.flickAngle);
+        }
+
+        //画面右をタップした時、右フリッパーを動かす。
+        if (Input.GetMouseButtonDown(0) && tag == "RightFripperTag" && Input.GetTouch(0).position.x > Screen.width * 0.5f) {
+            SetAngle(this.flickAngle);
+        }
+        //同時に押した場合
+        if (Input.GetMouseButtonDown(1)) {
+            SetAngle(this.flickAngle);
+        }
+
+        //指を画面から話した時、フリッパーをそれぞれ戻す。
+            if (Input.GetMouseButtonUp(0) && tag == "LeftFripperTag" &&  Input.GetTouch(0).phase == TouchPhase.Ended) {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.GetMouseButtonUp(0) && tag == "RightFripperTag"  && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            SetAngle(this.defaultAngle);
+        }
+        //同時に離した場合
+        if (Input.GetMouseButtonUp(1)){
+            SetAngle(this.defaultAngle);
+        }
+
+
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle) {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/ScoreText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour {
+    //スコアを定義、これをスコアにする。
+    private int Score = 0;
+    //スコアを表示するテキストを定義
+    private GameObject ballscore;
+
+	// Use this for initialization
+	void Start () {
+        //シーン中のBallScoreTextオブジェクトを取得
+        this.ballscore = GameObject.Find("ScoreText");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //スコアの表示
+        this.ballscore.GetComponent<Text>().text = "今日の貯金：" + this.Score;
+    }
+
+    //衝突時にそれぞれの得点を加算する関数を作成
+    void OnCollisionEnter(Collision other){
+        if (tag == "SmallStarTag"){
+            this.Score += 30;
+        }
+        if (tag == "SmallCloudTag") {
+            this.Score += 80;
+        }
+        if (tag == "LargeStarTag"){
+            this.Score += 10;
+        }
+        if (tag == "LargeCloudTag"){
+            this.Score += 1000;
+        }
+    }
+}
